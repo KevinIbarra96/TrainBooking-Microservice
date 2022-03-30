bab8f5d29664de4810aff64a2242eddd91e4eb65
Alejandra Escobar <ale.fesico@gmail.com>
2022-03-29 23:41:13 -0600

    Handle feign exceptions

--- TrainBooking/src/main/java/com/TrainBooking/app/constant/ApiConstants.java
+++ TrainBooking/src/main/java/com/TrainBooking/app/constant/ApiConstants.java
@@ -4,4 +4,5 @@ public class ApiConstants {
 	public static final String USER_NOT_FOUND = "U404";
 	public static final String INCORRECT_PASSWORD = "IP400";
 	public static final String INVALID_ARGS = "MA400";
+	public static final String FEIGN_INTERNAL_SERVER_ERROR = "F500";
 }
